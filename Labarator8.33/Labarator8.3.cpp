5a84a77ca2902cbf3e2dc66475825b71c87fce20
laim <ptiv.cs@mail.ru>
2026-02-22 17:12:16 +0700

    Добавьте файлы проекта.

---
+++ Labarator8.33/Labarator8.3.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "ru_RU.UTF-8");
+	const int SIZE = 10;
+	{
+		cout << "\n В-1 Статистический масив, индексная адресация " << endl;
+		int arr[SIZE];
+		for (int i = 0; i < SIZE; i++) {
+			arr[i] = i * i;
+		}
+		for (int i = 0; i < SIZE; i++) {
+			cout << "arr[" << i << "]=" << arr[i] << endl;
+		}
+	}
+	{
+		cout << "\n В-2 Статестический массив, косвенная адресация" << endl;
+		int arr[SIZE];
+		int* p = arr;
+		for (int i = 0; i < SIZE; i++) {
+			*(p + i) = i * i;
+
+		}
+		for (int i = 0; i < SIZE; i++) {
+			cout << "arr[" << i << "]=" << *(p + i) << endl;
+
+		}
+	}
+	{
+		cout << "\n В-3 Динамический массив, индексная адресация" << endl;
+		int* D = new int[SIZE];
+		for (int i = 0; i < SIZE; i++) {
+			D[i] = i * i;
+		}
+		for (int i = 0; i < SIZE; i++) {
+			cout << "Darr[" << i << "]=" << D[i] << endl;
+		}
+		delete[] D;
+	}
+	{
+		cout << "\n В=4 Динамический массив, косвенная адресация" << endl;
+		int* t = new int[SIZE];
+		for (int i = 0; i < SIZE; i++) {
+			*(t + i) = i * i;
+		}
+		for (int i = 0; i < SIZE; i++) {
+			cout << "Darr[" << i << "]=" << *(t + i) << endl;
+		}
+		delete[] t;
+	}
+	return 0;
+}
